8b7f7b7146ce103e68f313f6318d9b7c87203e14
fei11111 <455842151@qq.com>
2021-02-02 21:43:02 +0800

    完成自定义属性 完成模拟网络加载后布局重绘

--- app/src/main/java/com/fei/linechartview/LineChartView.java
+++ app/src/main/java/com/fei/linechartview/LineChartView.java
@@ -6,16 +6,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PointF;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
-import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.Nullable;
 
 /**
  * @ClassName: LineChartView
@@ -30,17 +28,15 @@ import java.util.List;
 public class LineChartView extends View {
 
     private final String TAG = "LineChartView";
-
+    //y轴坐标值
     private String[] mYMark = new String[]{
             "0.0", "4.0", "8.0", "12.0", "16.0", "20.0",
             "24.0", "28.0", "32.0", "36.0", "40.0", "44.0"
     };
 
-    private List<ChartData> mChartDatas;
-
-    private String[] mXMark;
-
+    //x,y轴,文字画笔
     private Paint mPaint;
+    //折线,圆形画笔
     private Paint mLinePaint;
 
     //字体大小
@@ -50,10 +46,10 @@ public class LineChartView extends View {
     //线颜色
     private int mLineColor = Color.RED;
     //x，y轴颜色
-    private int mNormalLineColor = Color.LTGRAY;
+    private int mAxleColor = Color.LTGRAY;
     //箭头高度
     private float mArrowHeight = 15f;
-    //箭头宽度一半
+    //箭头宽度一半,箭头高度/tan60°
     private float mArrowHalfWidth;
     //箭头间距
     private float mArrowSpace = 20f;
@@ -63,21 +59,23 @@ public class LineChartView extends View {
     private float mXFontHorizontalSpace = 20;
     //x轴字体垂直间距
     private float mXFontVerticalSpace = 8;
+    //圆半径
+    private float mCircleRadius = 5;
     //y轴每份的高度大小
     private float mPerY;
     //x轴每份的宽度大小
     private float mPerX;
-
-    private int mFontHeight;//字体高度
-    private int mFontWidth;//字体宽度
+    //y轴字体最大宽度
+    private int mYFontWidth;
+    //x轴字体宽度
+    private int mXFontWidth;
+    //x轴字体高度
+    private int mXFontHeight;
     private Rect mTextBounds;
-
     private float mMarkLineWidth = 8f;//刻度线长度
 
-    //圆半径
-    private float mCircleRadius = 5;
-    //点坐标
-    private List<PointF> mPointFs = new ArrayList<>();
+    //后台返回值
+    private List<ChartData> mChartDatas;
 
     public LineChartView(Context context) {
         this(context, null);
@@ -92,9 +90,28 @@ public class LineChartView extends View {
 
         //获取属性值，后面在增加属性
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LineChartView);
+        //字体大小
         mFontSize = typedArray.getDimension(R.styleable.LineChartView_fontSize, sp2px(mFontSize));
+        //线宽度
         mLineWidth = typedArray.getDimension(R.styleable.LineChartView_lineWidth, dp2px(mLineWidth));
+        //线颜色
         mLineColor = typedArray.getColor(R.styleable.LineChartView_lineColor, mLineColor);
+        //x，y轴颜色
+        mAxleColor = typedArray.getColor(R.styleable.LineChartView_axleColor, mAxleColor);
+        //箭头高度
+        mArrowHeight = typedArray.getDimension(R.styleable.LineChartView_arrowHeight, dp2px(mArrowHeight));
+        //箭头距离刻度间距
+        mArrowSpace = typedArray.getDimension(R.styleable.LineChartView_arrowSpace, dp2px(mArrowSpace));
+        //y轴字体水平间距
+        mYFontHorizontalSpace = typedArray.getDimension(R.styleable.LineChartView_yFontHorizontalSpace, dp2px(mYFontHorizontalSpace));
+        //x轴字体水平间距
+        mXFontHorizontalSpace = typedArray.getDimension(R.styleable.LineChartView_xFontHorizontalSpace, dp2px(mXFontHorizontalSpace));
+        //x轴字体垂直间距
+        mXFontVerticalSpace = typedArray.getDimension(R.styleable.LineChartView_xFontVerticalSpace, dp2px(mXFontVerticalSpace));
+        //刻度宽度
+        mMarkLineWidth = typedArray.getDimension(R.styleable.LineChartView_markLineWidth, dp2px(mMarkLineWidth));
+        //圆点半径
+        mCircleRadius = typedArray.getDimension(R.styleable.LineChartView_circleRadius, dp2px(mCircleRadius));
         typedArray.recycle();
 
         //画普通x，y轴和标注的画笔
@@ -102,7 +119,7 @@ public class LineChartView extends View {
         mPaint.setDither(true);
         mPaint.setStrokeWidth(mLineWidth);
         mPaint.setTextSize(mFontSize);
-        mPaint.setColor(mNormalLineColor);
+        mPaint.setColor(mAxleColor);
 
         //画折线的画笔
         mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -111,43 +128,11 @@ public class LineChartView extends View {
         mLinePaint.setTextSize(mFontSize);
         mLinePaint.setColor(mLineColor);
 
-        //箭头高度
-        mArrowHeight = dp2px(mArrowHeight);
-        //箭头距离刻度间距
-        mArrowSpace = dp2px(mArrowSpace);
-        //y轴字体水平间距
-        mYFontHorizontalSpace = dp2px(mYFontHorizontalSpace);
-        //x轴字体水平间距
-        mXFontHorizontalSpace = dp2px(mXFontHorizontalSpace);
-        //x轴字体垂直间距
-        mXFontVerticalSpace = dp2px(mXFontVerticalSpace);
-        //刻度宽度
-        mMarkLineWidth = dp2px(mMarkLineWidth);
-
         //文本矩形
         mTextBounds = new Rect();
-
         //箭头宽度一半
         mArrowHalfWidth = (float) (mArrowHeight / Math.tan(Math.PI / 3));
 
-        //圆点半径
-        mCircleRadius = dp2px(mCircleRadius);
-
-        //测试数据
-        mChartDatas = new ArrayList<>();
-        ChartData chartData = new ChartData("2020-01-10", "1.0");
-        ChartData chartData1 = new ChartData("2020-01-11", "11.0");
-        ChartData chartData2 = new ChartData("2020-01-12", "9.0");
-        ChartData chartData3 = new ChartData("2020-01-13", "30.0");
-        ChartData chartData4 = new ChartData("2020-01-14", "42.0");
-        ChartData chartData5 = new ChartData("2020-01-15", "25.0");
-
-        mChartDatas.add(chartData);
-        mChartDatas.add(chartData1);
-        mChartDatas.add(chartData2);
-        mChartDatas.add(chartData3);
-        mChartDatas.add(chartData4);
-        mChartDatas.add(chartData5);
     }
 
     private float dp2px(float dp) {
@@ -160,26 +145,34 @@ public class LineChartView extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        //mPerY = (总高度-padding-箭头间距-箭头高度)/Y刻度总数
-        mPerY = (getMeasuredHeight() - getPaddingTop() - getPaddingBottom() - mArrowSpace - mArrowHeight) / mYMark.length;
-        //取最大值的高宽度
+        if (mChartDatas == null || mChartDatas.size() == 0) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+        //测量总宽度
+        //取y轴刻度值最大值的高宽度
         mPaint.getTextBounds(mYMark[mYMark.length - 1], 0, mYMark[mYMark.length - 1].length(), mTextBounds);
-        mFontHeight = mTextBounds.height();
-        mFontWidth = mTextBounds.width();
+        mYFontWidth = mTextBounds.width();
         String key = mChartDatas.get(0).getKey();
         mPaint.getTextBounds(key, 0, key.length(), mTextBounds);
-        float textWidth = mTextBounds.width();
+
+        mXFontWidth = mTextBounds.width();
+        mXFontHeight = mTextBounds.height();
         //宽度重新设置，使之可以滑动
-        int width = (int) (mYFontHorizontalSpace + mFontWidth + mYFontHorizontalSpace + mLineWidth + (mXFontHorizontalSpace * 2 + textWidth)
+        //宽度=padding+y轴水平间距+字体宽度+y轴水平间距+y轴线宽度+x轴每段文本和间距宽度*x轴刻度数量+箭头间距+箭头高度
+        int width = (int) (getPaddingLeft() + getPaddingRight() + mYFontHorizontalSpace + mYFontWidth + mYFontHorizontalSpace + mLineWidth + (mXFontHorizontalSpace * 2 + mXFontWidth)
                 * mChartDatas.size() + mArrowSpace + mArrowHeight);
         setMeasuredDimension(width, heightMeasureSpec);
 
+        //y轴每段大小 = (总高度-padding-箭头间距-箭头高度)/Y刻度总数
+        mPerY = (getMeasuredHeight() - getPaddingTop() - getPaddingBottom() - mArrowSpace - mArrowHeight) / mYMark.length;
+        //x轴每段大小 = x轴文本间距*2+文本宽度
+        mPerX = mXFontWidth + mXFontHorizontalSpace * 2;
     }
-
 
     @Override
     protected void onDraw(Canvas canvas) {
-        mPointFs.clear();
+        if (mChartDatas == null || mChartDatas.size() == 0) return;
         int paddingTop = getPaddingTop();
         int paddingLeft = getPaddingLeft();
         //1.画y轴
@@ -187,7 +180,7 @@ public class LineChartView extends View {
         //文本左边
         float yTextLeft = paddingLeft + mYFontHorizontalSpace;
         //y轴左边
-        float yLeft = yTextLeft + mFontWidth + mYFontHorizontalSpace;
+        float yLeft = yTextLeft + mYFontWidth + mYFontHorizontalSpace;
         //y轴上边
         float yTop = paddingTop + mArrowHeight;
         //文本上边
@@ -195,77 +188,80 @@ public class LineChartView extends View {
         Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
         //baseline差距
         float dy = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+        //1.1.1 画刻度和刻度值
         for (int i = 0; i < mYMark.length; i++) {
             String text = mYMark[mYMark.length - i - 1];
-            //刻度
-            canvas.drawLine(yLeft, yTextTop + i * mPerY, yLeft + mMarkLineWidth, yTop + mArrowSpace + i * mPerY, mPaint);
+            float yMarkTop = yTextTop + i * mPerY;
+            canvas.drawLine(yLeft, yMarkTop, yLeft + mMarkLineWidth, yMarkTop, mPaint);
             mPaint.getTextBounds(text, 0, text.length(), mTextBounds);
             float textWidth = mTextBounds.width();
             //与最大字的宽度差距
-            float distance = (mFontWidth - textWidth) / 2;
+            float distance = (mYFontWidth - textWidth) / 2;
             //画文本
-            canvas.drawText(text, yTextLeft + distance, yTextTop + i * mPerY + dy, mPaint);
+            canvas.drawText(text, yTextLeft + distance, yMarkTop + dy, mPaint);
         }
-        //1.2y轴线
-        canvas.drawLine(yLeft, yTop, yLeft, yTop + mArrowSpace + (mYMark.length - 1) * mPerY, mPaint);
-        //1.3y轴箭头
+        //1.2 y轴线
+        //x轴的top刚好等于y轴的bottom
+        float yBottom = yTop + mArrowSpace + (mYMark.length - 1) * mPerY;
+        canvas.drawLine(yLeft, yTop, yLeft, yBottom, mPaint);
+        //1.3 y轴箭头
         float arrowLeft = yLeft;
         float arrowTop = yTop;
         drawTriangle(arrowLeft, arrowTop, arrowLeft, arrowTop - mArrowHeight, mArrowHeight,
                 mArrowHalfWidth, canvas, mPaint);
 
         //2.画x轴
-        //2.1获取字体高宽度
-        float xTop = yTop + mArrowSpace + (mYMark.length - 1) * mPerY;
-        String key = mChartDatas.get(0).getKey();
-        mPaint.getTextBounds(key, 0, key.length(), mTextBounds);
-        float textWidth = mTextBounds.width();
-        float textHeight = mTextBounds.height();
+        //x轴的top刚好等于y轴的bottom
+        float xTop = yBottom;
         //x轴宽度=文本宽度+文本水平宽度*2+箭头间距
         canvas.drawLine(yLeft, xTop,
-                yLeft + mChartDatas.size() * (textWidth + mXFontHorizontalSpace * 2) + mArrowSpace,
+                yLeft + mChartDatas.size() * mPerX + mArrowSpace,
                 xTop, mPaint);
         //2.2画x轴刻度
         //记录之前点坐标
-        float previousDataX = 0;
-        float previousDataY = 0;
+        float previousCircleX = 0;
+        float previousCircleY = 0;
         for (int i = 0; i < mChartDatas.size(); i++) {
-            //记录每个点的坐标
-            //刻度
-            float dataX = yLeft + (mXFontHorizontalSpace * 2 + textWidth) * i + textWidth / 2 + mXFontHorizontalSpace;
-            canvas.drawLine(dataX,
+            //画刻度
+            float circleX = yLeft + mPerX * i + mXFontWidth / 2 + mXFontHorizontalSpace;
+            canvas.drawLine(circleX,
                     xTop - mMarkLineWidth,
-                    yLeft + (mXFontHorizontalSpace * 2 + textWidth) * i + textWidth / 2 + mXFontHorizontalSpace,
+                    yLeft + mPerX * i + mXFontWidth / 2 + mXFontHorizontalSpace,
                     xTop,
                     mPaint);
             String xValue = mChartDatas.get(i).getKey();
-            canvas.drawText(xValue, yLeft + mXFontHorizontalSpace * (i + 1) + (textWidth + mXFontHorizontalSpace) * i,
-                    xTop + mXFontVerticalSpace + textHeight / 2 + dy, mPaint);
-            //将值转成int类型，除以4，并且取余
+            canvas.drawText(xValue, yLeft + mXFontHorizontalSpace + mPerX * i,
+                    xTop + mXFontVerticalSpace + mXFontHeight / 2 + dy, mPaint);
+            //将值转成float类型，除以4，并且取余
             float value = Float.parseFloat(mChartDatas.get(i).getValue());
+            //获取坐标在y轴哪一段，但是y轴是倒序，还需要mYMark.length - 1 - yIndex才是最终哪一段
             int yIndex = (int) (value / 4);
+            //获取y轴哪一段后的一点偏移量
             float offset = value % 4 * mPerY / 4;
-            float dataY = yTextTop + (mYMark.length - 1 - yIndex) * mPerY - offset;
+            //圆点y轴坐标=y轴位置坐标+(y轴总段数-1-当前段数)*每段大小-偏移量
+            float circleY = yTextTop + (mYMark.length - 1 - yIndex) * mPerY - offset;
             //3.画点
-            canvas.drawCircle(dataX, dataY, mCircleRadius, mLinePaint);
+            canvas.drawCircle(circleX, circleY, mCircleRadius, mLinePaint);
             //4.点连线
-            if (previousDataX != 0) {
-                canvas.drawLine(previousDataX, previousDataY, dataX, dataY, mLinePaint);
+            if (previousCircleX != 0) {
+                canvas.drawLine(previousCircleX, previousCircleY, circleX, circleY, mLinePaint);
             }
-            previousDataX = dataX;
-            previousDataY = dataY;
+            //报到为之前坐标，用于画折线
+            previousCircleX = circleX;
+            previousCircleY = circleY;
             //5.画点上的值
             mPaint.getTextBounds(mChartDatas.get(i).getValue(), 0, mChartDatas.get(i).getValue().length(), mTextBounds);
-            textWidth = mTextBounds.width();
-            textHeight = mTextBounds.height();
+            float tipTextWidth = mTextBounds.width();
+            float tipTextHeight = mTextBounds.height();
             // 圆点x位置-文本一半
-            float textLeft = dataX - textWidth / 2;
-            float textTop = dataY - mCircleRadius - textHeight / 2;
+            float textLeft = circleX - tipTextWidth / 2;
+            //文本y轴坐标 = 圆点y轴坐标-圆半径-x轴垂直间距-文本高度一半，还需要加上dy
+            float textTop = circleY - mCircleRadius - mXFontVerticalSpace - tipTextHeight / 2;
             canvas.drawText(mChartDatas.get(i).getValue(), textLeft, textTop + dy, mPaint);
         }
         //2.3x轴箭头
         //x轴箭头left = x轴right+箭头间距
-        float xArrowLeft = yLeft + (mXFontHorizontalSpace * 2 + textWidth) * mChartDatas.size() + mArrowSpace;
+        float xArrowLeft = yLeft + mPerX * mChartDatas.size() + mArrowSpace;
         float xArrowTop = xTop;
         drawTriangle(xArrowLeft,
                 xArrowTop, xArrowLeft + mArrowHeight, xArrowTop,
@@ -312,7 +308,8 @@ public class LineChartView extends View {
         return Math.sqrt(dx * dx + dy * dy);
     }
 
-    public void setXMark(String[] xMark) {
-        this.mXMark = xMark;
+    public void setData(List chartDatas) {
+        this.mChartDatas = chartDatas;
+        requestLayout();
     }
 }
--- app/src/main/java/com/fei/linechartview/MainActivity.java
+++ app/src/main/java/com/fei/linechartview/MainActivity.java
@@ -1,8 +1,13 @@
 package com.fei.linechartview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final LineChartView lineChartView = findViewById(R.id.line_chart_view);
+
+        //测试数据
+        final List mChartDatas = new ArrayList<>();
+        ChartData chartData = new ChartData("2020-01-10", "1.0");
+        ChartData chartData1 = new ChartData("2020-01-11", "11.0");
+        ChartData chartData2 = new ChartData("2020-01-12", "9.0");
+        ChartData chartData3 = new ChartData("2020-01-13", "30.0");
+        ChartData chartData4 = new ChartData("2020-01-14", "42.0");
+        ChartData chartData5 = new ChartData("2020-01-15", "25.0");
+
+        mChartDatas.add(chartData);
+        mChartDatas.add(chartData1);
+        mChartDatas.add(chartData2);
+        mChartDatas.add(chartData3);
+        mChartDatas.add(chartData4);
+        mChartDatas.add(chartData5);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                lineChartView.setData(mChartDatas);
+            }
+        },5000);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity","onResume");
     }
 }
